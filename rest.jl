c119fb6d7c262d3a1262de1a1a3008e1608c6d66
Ole Kröger <o.kroeger@opensourc.es>
2023-07-26 22:04:54 +0200

    added subscribe for Strava authentication

--- rest.jl
+++ rest.jl
@@ -1,10 +1,19 @@
+import DotEnv
 using Genie
+import Genie.Requests: getpayload
 import Genie.Renderer.Json: json
 
+DotEnv.config()
 Genie.config.run_as_server = true
+VERIFY_TOKEN = ENV["VERIFY_TOKEN"]
 
-route("/") do
-  (:message => "Hi there!") |> json
+route("/subscribe") do
+    verify_token = getpayload(Symbol("hub.verify_token"))
+    if verify_token != VERIFY_TOKEN
+        return Genie.Responses.setstatus(401)
+    end
+    d = Dict{String,String}("hub.challenge" => getpayload(Symbol("hub.challenge")))
+    d |> json
 end
 
-up()
+up(8000, "0.0.0.0")
