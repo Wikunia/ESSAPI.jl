d0dcd272cfb714f15cef50e1afbdd03d668ff7d7
Ole Kröger <o.kroeger@opensourc.es>
2023-07-26 23:23:03 +0200

    get params with default

--- rest.jl
+++ rest.jl
@@ -7,11 +7,11 @@ VERIFY_TOKEN = ENV["VERIFY_TOKEN"]
 
 @get "/subscribe" function(req::HTTP.Request)
     params = queryparams(req)
-    verify_token = params["hub.verify_token"]
+    verify_token = get(params, "hub.verify_token", "")
     if verify_token != VERIFY_TOKEN
         return html(""; status=401)
     end
-    return Dict{String,String}("hub.challenge" => params["hub.challenge"])
+    return Dict{String,String}("hub.challenge" => get(params,"hub.challenge", "not available"))
 end
 
 serve(host="0.0.0.0", port=8000)
