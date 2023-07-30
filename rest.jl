08fa753f68514e91185cb18f18b4f6b2a343898d
Ole Kröger <o.kroeger@opensourc.es>
2023-07-30 21:16:38 +0200

    receive post data

--- rest.jl
+++ rest.jl
@@ -1,6 +1,7 @@
 import DotEnv
 using Oxygen
 using HTTP
+using JSON3
 
 DotEnv.config()
 VERIFY_TOKEN = ENV["VERIFY_TOKEN"]
@@ -12,6 +13,14 @@ VERIFY_TOKEN = ENV["VERIFY_TOKEN"]
         return html(""; status=401)
     end
     return Dict{String,String}("hub.challenge" => get(params,"hub.challenge", "not available"))
+end
+
+@post "/subscribe" function(req::HTTP.Request)
+    params = queryparams(req)
+    open("latest_data.json", "w") do io
+        JSON3.pretty(io, params)
+    end
+    return "EVENT_RECEIVED"
 end
 
 serve(host="0.0.0.0", port=8000)
