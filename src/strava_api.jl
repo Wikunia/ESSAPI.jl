bdff5e7d277c98e6b8323ec10737218a5396df5d
Ole Kröger <o.kroeger@opensourc.es>
2023-08-11 21:30:53 +0200

    download only ones, save nodes and store xml

--- src/strava_api.jl
+++ src/strava_api.jl
@@ -31,6 +31,8 @@ end
 download_activity(user_id::Int, activity_id) = download_activity(user_id, get_access_token(user_id), activity_id)
 
 function download_activity(user_id, access_token, activity_id, start_time)
+    path = joinpath(DATA_FOLDER, "activities", "$user_id", "$activity_id.json")
+    isfile(path) && return false
     url = "https://www.strava.com/api/v3/activities/$activity_id/streams?keys=latlng,time"
 
     headers = Dict("Authorization" => "Bearer $access_token", "Content-Type" => "application/json")
@@ -48,11 +50,12 @@ function download_activity(user_id, access_token, activity_id, start_time)
     end
 
     save_data[:start_time] = start_time
-    path = joinpath(DATA_FOLDER, "activities", "$user_id", "$activity_id.json")
+    
     mkpath(dirname(path))
     open(path, "w") do io
         JSON3.pretty(io, save_data)
     end
+    return true
 end
 
 function set_activity_fields(access_token, activity_id, payload)
@@ -80,16 +83,20 @@ function add_activity(user_id, activity_id)
     access_token = get_access_token(user_id)
     activity_data = get_activity_data(access_token, activity_id)
     start_time = activity_data[:start_date]
-    download_activity(user_id, access_token, activity_id, start_time)
+    download_activity(user_id, access_token, activity_id, start_time) || return
     
     activity_path = joinpath(DATA_FOLDER, "activities", "$user_id", "$activity_id.json")
     user_data = readjson(joinpath(DATA_FOLDER, "user_data", "$user_id.json"))
     city_data_path = joinpath(DATA_FOLDER, "city_data", "$user_id", "$(user_data[:city_name]).jld2")
     city_data = load(city_data_path)
     data = EverySingleStreet.map_matching(activity_path, city_data["ways"], city_data["walked_parts"], "tmp_local_map.json")
+    rm("tmp_local_map.json")
 
     added_kms_str = @sprintf "Added road kms: %.2f km" data.added_kms
-    save(city_data_path, Dict("ways" => city_data["ways"], "walked_parts" => data.walked_parts))
+    save(city_data_path, Dict("nodes" => city_data["nodes"], "ways" => city_data["ways"], "walked_parts" => data.walked_parts))
+
+    walked_xml_path = joinpath(DATA_FOLDER, "city_data", "$user_id", "$(user_data[:city_name])_walked.xml")
+    EverySingleStreet.create_xml(city_data["nodes"], data.walked_parts, walked_xml_path)
 
     prepend_activity_description(access_token, activity_data, added_kms_str)
 end
